9d36df8170f1d0745103325dd66e0439ce5a5e0d
Lucbibiano <lukasssb@hotmail.com>
2026-04-18 15:29:46 -0300

    feat: adding Cognito settings

---
+++ amplify.config.ts
@@ -0,0 +1,12 @@
+import { Amplify } from 'aws-amplify';
+
+export function configureAmplify() {
+  Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId: '',
+        userPoolClientId: ''
+      }
+    }
+  })
+}
